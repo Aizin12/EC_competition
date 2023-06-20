c5f2642100fcae45c3a87ef1ce8f1a1f76e5110b
Aizin12 <absolutely1725@gmail.com>
2023-06-20 09:29:01 +0900

    add function

--- differential_evolution.py
+++ differential_evolution.py
@@ -1,1 +1,20 @@
+import numpy as np
+
+#並列化適応済
 def rastringen (arg):
+    sigma = np.zeros(len(arg))
+    sigma = arg**2 - 10*np.cos(2*np.pi*arg)
+    return 10*len(arg)+sigma.sum()
+
+#for文で計算
+def rosenbrock (arg):
+    sigma = 0
+    for i in range(1,len(arg)-1):
+        sigma += 100*(arg[i+1]-arg[i]**2)**2 + (arg[i]-1)**2
+    return sigma
+
+
+arg = np.zeros(10)
+arg = 10.12*np.random.rand(10)-5.12
+result =rosenbrock(arg)
+print(result)
