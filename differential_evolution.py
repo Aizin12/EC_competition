a651da0b414645f7fe7c0e1ed1c6fe1515eecbe4
Aizin12 <absolutely1725@gamil.com>
2023-06-20 10:06:03 +0900

    no comment

--- differential_evolution.py
+++ differential_evolution.py
@@ -15,6 +15,25 @@ def rosenbrock (arg):
 
 
 arg = np.zeros(10)
-arg = 10.12*np.random.rand(10)-5.12
-result =rosenbrock(arg)
-print(result)
+arg2 = np.zeros(10)
+average = 0
+average2 = 0
+
+NUM = 10**6
+for i in range(1,32):
+    np.random.seed(i-1)
+    for j in range(1,NUM+1):
+        arg = 10.24*np.random.rand(10)-5.12
+        arg2 = 4.096*np.random.rand(10) -2.048
+
+        result1 = rastringen(arg)
+        result2 = rosenbrock(arg2)
+        average += result1
+        average2 += result2
+    average /= NUM
+    average2 /= NUM
+
+average /= 31
+average2 /= 31
+print(average)
+print(average2)
