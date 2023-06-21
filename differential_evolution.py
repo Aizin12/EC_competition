21cba8e9f0bb54da3928caaaa83c94d7c5f3d6a5
Aizin12 <absolutely1725@gmail.com>
2023-06-21 19:35:02 +0900

    06/21

--- differential_evolution.py
+++ differential_evolution.py
@@ -1,39 +1,44 @@
 import numpy as np
 
-#並列化適応済
-def rastringen (arg):
-    sigma = np.zeros(len(arg))
-    sigma = arg**2 - 10*np.cos(2*np.pi*arg)
-    return 10*len(arg)+sigma.sum()
+N = 10
 
-#for文で計算
-def rosenbrock (arg):
-    sigma = 0
-    for i in range(1,len(arg)-1):
-        sigma += 100*(arg[i+1]-arg[i]**2)**2 + (arg[i]-1)**2
-    return sigma
+class objective_function:
+    def __init__(self,array):
+        self.array = array
+
+    #並列化適応済
+    def rastringen(self):
+        sigma = np.zeros(len(self.array))
+        sigma = arg**2 - 10*np.cos(2*np.pi*self.array)
+        return 10*len(self.array)+sigma.sum()
+
+    #for文で計算
+    def rosenbrock(self):
+        sigma = 0
+        for i in range(1,len(self.array)-1):
+            sigma += 100*(self.array[i+1]-self.array[i]**2)**2 + (self.array[i]-1)**2
+        return sigma
+
+    #計算の正確性の確認
+    def  checkfunction(self):
+        f = objective_function(np.zeros(10))
+        print(f.rosenbrock())
+        
+class DifferentialEvolutin(self):
+    def __init__(self,NP,F,CR,f_type):
+        self.NP =NP #母集団サイズ
+        self.F = F  #差分重量
+        self.CR = CR #交差率
+        self.f_type = f_type #評価関数の種類
+
+    ind = np.zeros(self.NP,N)
+
+    #初期集団生成
+    def initialization(self):
+        for i in range(0,self.NP)
+
+
 
 
 arg = np.zeros(10)
 arg2 = np.zeros(10)
-average = 0
-average2 = 0
-
-NUM = 10**6
-for i in range(1,32):
-    np.random.seed(i-1)
-    for j in range(1,NUM+1):
-        arg = 10.24*np.random.rand(10)-5.12
-        arg2 = 4.096*np.random.rand(10) -2.048
-
-        result1 = rastringen(arg)
-        result2 = rosenbrock(arg2)
-        average += result1
-        average2 += result2
-    average /= NUM
-    average2 /= NUM
-
-average /= 31
-average2 /= 31
-print(average)
-print(average2)
