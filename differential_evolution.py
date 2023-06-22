6cb7b949c7f84eef1e475e7c34eed8d424113e1c
Aizin12 <absolutely1725@gmail.com>
2023-06-22 13:09:27 +0900

    初期集団生成,評価追加

--- differential_evolution.py
+++ differential_evolution.py
@@ -1,44 +1,50 @@
 import numpy as np
 
+#rastrigen関数,rosenbrock関数の次元の数
 N = 10
 
-class objective_function:
-    def __init__(self,array):
-        self.array = array
+#並列化適応済
+def rastrigen(array):
+        sigma = np.zeros(len(array))
+        sigma = array**2 - 10*np.cos(2*np.pi*array)
+        return 10*len(array)+sigma.sum()
 
-    #並列化適応済
-    def rastringen(self):
-        sigma = np.zeros(len(self.array))
-        sigma = arg**2 - 10*np.cos(2*np.pi*self.array)
-        return 10*len(self.array)+sigma.sum()
+#for文で計算
+def rosenbrock(array):
+    sigma = 0
+    for i in range(1,len(array)-1):
+       sigma += 100*(array[i+1]-array[i]**2)**2 + (array[i]-1)**2
+    return sigma
 
-    #for文で計算
-    def rosenbrock(self):
-        sigma = 0
-        for i in range(1,len(self.array)-1):
-            sigma += 100*(self.array[i+1]-self.array[i]**2)**2 + (self.array[i]-1)**2
-        return sigma
-
-    #計算の正確性の確認
-    def  checkfunction(self):
-        f = objective_function(np.zeros(10))
-        print(f.rosenbrock())
+#計算の正確性の確認
+def  checkfunction():
+    f = objective_function(np.zeros(N))
+    print(f.rosenbrock())
         
-class DifferentialEvolutin(self):
-    def __init__(self,NP,F,CR,f_type):
+#遺伝子
+class DifferentialEvolutin:
+    def __init__(self,NP,F,CR,ftype):
         self.NP =NP #母集団サイズ
         self.F = F  #差分重量
         self.CR = CR #交差率
-        self.f_type = f_type #評価関数の種類
-
-    ind = np.zeros(self.NP,N)
+        self.ftype = ftype #目的関数の種類
+        self.ind = np.zeros((NP,N)) 
 
     #初期集団生成
-    def initialization(self):
-        for i in range(0,self.NP)
+    def initializate(self):
+        self.ind = np.random.rand(self.NP,N)
+
+    #評価 
+    def evaluate(self):
+        result = np.zeros(self.NP)
+        for i in range(0,self.NP):
+            result = self.ftype(self.ind[i])
+        return np.amax(result)    
 
 
 
 
-arg = np.zeros(10)
-arg2 = np.zeros(10)
+a = DifferentialEvolutin(10,1,1,rastrigen)
+a.initializate()
+result = a.evaluate()
+print(result)
