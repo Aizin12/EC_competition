a8c50210adb35c5f593cc3b3138989ea4af15555
Aizin12 <absolutely1725@gmail.com>
2023-06-27 21:53:20 +0900

    突然変異,交差 修正

--- differential_evolution.py
+++ differential_evolution.py
@@ -4,7 +4,7 @@ import random
 
 #-----パラメーター------
 
-N = 10  #rastrigen関数,rosenbrock関数の次元の数
+D = 10  #rastrigen関数,rosenbrock関数の次元の数
 num_E = 10**6
 
 
@@ -26,10 +26,10 @@ def rosenbrock(array):  #for文で計算
 #計算の正確性の確認
 def  checkfunction(func):
     np.random.seed(0)
-    array = np.zeros(N)
-    array = np.random.rand(N)
+    array = np.zeros(D)
+    array = np.random.rand(D)
     result = func(array)
-    array = array.reshape(1,N)
+    array = array.reshape(1,D)
     with open('check_function.csv','at') as cfile:
         np.savetxt(cfile,[func.__name__],fmt="%s",newline="\n")
         np.savetxt(cfile,array,delimiter=',')
@@ -46,14 +46,14 @@ class FuncDE:
         self.Y = Y
         self.CR = CR #交差率
         self.ftype = ftype #目的関数の種類
-        self.ind = np.zeros((NP,N)) #遺伝子
-        self.target_verctor = np.zeros(N) #ターゲットベクトル
-        self.v = np.zeros(N) #変異ベクトル
-        self.u = np.zeros(N) #トライアルベクトル
+        self.ind = np.zeros((NP,D)) #遺伝子
+        self.base_verctor = np.zeros(D) #ベースベクトル
+        self.v = np.zeros(D) #変異ベクトル
+        self.u = np.zeros(D) #トライアルベクトル
 
     #初期集団生成
     def initializate(self):
-        self.ind = np.random.rand(self.NP,N)
+        self.ind = np.random.rand(self.NP,D)
 
     #評価 
     def evaluate(self):
@@ -64,37 +64,28 @@ class FuncDE:
 
     #乱数によるベースベクトルの選択
     def random_base_select(self):
-        self.target_verctor = self.ind[np.random.randint(0,self.NP)]
+        self.base_verctor = self.ind[np.random.randint(0,self.NP)]
 
     #差分突然変異
     def mutation(self):
         #重複のない乱数生成
-        checker = np.zeros(self.NP)
-        select = np.zeros(2*self.Y)
-        loop_cnt = 0
-        cnt = 0
-        while(cnt<2*self.Y):
-            if(np.random.rand()<1/self.NP) and (checker[loop_cnt] == 0):
-                select[cnt] = loop_cnt
-                checker[loop_cnt] = 1
-                cnt += 1
-            
-            loop_cnt += 1
-            if(loop_cnt >= self.NP):
-                loop_cnt = 0
+        select = np.random.permutation(self.NP)
+        select = select[:2*self.Y]
         
         sigma = np.zeros(N)
         for i in range(self.Y):
             sigma += (self.ind[int(select[2*i+1])] - self.ind[int(select[2*i])])
 
-        self.v = self.target_verctor + self.F*sigma
+        self.v = self.base_verctor + self.F*sigma
         
     #二項交差
     def  binomial_crossover(self):
-        self.u = self.target_verctor
-        r = np.random.rand()
-        if(r < self.CR):
-            self.u = self.v
+        self.u = self.base_verctor
+        jr = np.random.randint(D)
+        for j in range(D):
+            r = np.random.rand()
+            if(r < self.CR) or (j == jr):
+                self.u = self.v
         return self.u
         
 
@@ -106,7 +97,10 @@ def DifferentialEvolution(X,Y,Z,NP,F,CR,ftype):
     evolution.initializate()
     
     for g in range(G):
-        evolution.X()
 
 
-
+x = FuncDE(100,1,2,1,rosenbrock)
+x.initializate()
+x.random_base_select()
+x.mutation()
+print(x.v)
