efb96d91f87869046bc6b99aeeb0e86dbe771455
Aizin12 <absolutely1725@gmail.com>
2023-07-02 20:25:35 +0900

    原始差分進化の実装及びテスト環境の実装

--- differential_evolution.py
+++ differential_evolution.py
@@ -24,8 +24,8 @@ def rosenbrock(array):  #for文で計算
     return sigma
 
 #計算の正確性の確認
-def  checkfunction(func):
-    np.random.seed(0)
+def  check_function(func,seed):
+    np.random.seed(seed)
     array = np.zeros(D)
     array = np.random.rand(D)
     result = func(array)
@@ -42,14 +42,16 @@ def  checkfunction(func):
 class DifferentialEvolution:
     def __init__(self,NP,F,Y,CR,target):
         self.NP =NP #母集団サイズ
-        self.F = F  #差分重量
+        self.F = F  #スケーリングサイズ
         self.Y = Y
         self.CR = CR #交差率
         self.target = target #目的関数の種類
         self.ind = np.zeros((NP,D)) #遺伝子
         self.base_verctor = np.zeros(D) #ベースベクトル
+        self.target_vector = np.zeros(D) #ターゲットベクトル
         self.v = np.zeros(D) #変異ベクトル
         self.u = np.zeros(D) #トライアルベクトル
+        self.path = f"./{target.__name__}/DE_{NP}_{F}_{Y}_{CR}.csv" #結果格納用アドレス名
 
     #初期集団生成
     def initializate(self):
@@ -58,13 +60,13 @@ class DifferentialEvolution:
     #評価 
     def evaluate(self):
         result = np.zeros(self.NP)
-        for i in range(0,self.NP):
-            result = self.target(self.ind[i])
-        return np.amax(result)
+        for i in range(self.NP):
+            result[i] = self.target(self.ind[i])
+        return np.amin(result)
 
     #乱数によるベースベクトルの選択
     def random_base_select(self):
-        self.base_verctor = self.ind[np.random.randint(0,self.NP)]
+        self.base_verctor = self.ind[np.random.randint(0,self.NP)].copy()
 
     #差分突然変異
     def mutation(self):
@@ -74,55 +76,74 @@ class DifferentialEvolution:
         
         sigma = np.zeros(D)
         for i in range(self.Y):
-            sigma += (self.ind[int(select[2*i+1])] - self.ind[int(select[2*i])])
+            sigma += (self.ind[int(select[2*i])] - self.ind[int(select[2*i+1])])
 
         self.v = self.base_verctor + self.F*sigma
         
     #二項交差
     def  binomial_crossover(self):
-        self.u = self.base_verctor
+        self.u = self.target_vector.copy()
         jr = np.random.randint(D)
         for j in range(D):
             r = np.random.rand()
             if(r < self.CR) or (j == jr):
-                self.u = self.v
+                if(self.v[j] < 0):
+                    self.u[j] = self.base_verctor[j] + np.random.rand()*(-self.base_verctor[j])
+                elif (self.v[j] > 1):
+                    self.u[j] = self.base_verctor[j] + np.random.rand()*(1-self.base_verctor[j])
+                else:
+                    self.u[j] = self.v[j].copy()
 
+
+#
     def DE_random_Y_binomial(self):
-        result = np.zeros(int(G))
+        result = np.zeros(int(G)+1)
 
         self.initializate()
+        result[0] = self.evaluate()
     
         for g in range(int(G)):
             for i in range(self.NP):
-                target_vector = self.ind[i]
+                self.target_vector = self.ind[i].copy()
                 self.random_base_select()
                 self.mutation()
                 self.binomial_crossover()
-                if (self.target(self.u) < self.target(target_vector)):
-                    self.ind[i] = self.u
-            result[g] = self.evaluate()
-
+                if (self.target(self.u) < self.target(self.target_vector)):
+                    self.ind[i] = self.u.copy()
+            result[g+1] = self.evaluate()
         return result
 
 
-    
+#
+class  Runner:
+    def __init__(self,NP,F,Y,CR,target):
+        self.NP =NP #母集団サイズ
+        self.F = F  #スケーリングサイズ
+        self.Y = Y  #差ベクトルの数
+        self.CR = CR #交差率
+        self.target = target #目的関数の種類
+        
+    def DE_random_Y_binomial(self):
+        evolution = DifferentialEvolution(self.NP,self.F,self.Y,self.target,self.target)
+        G =  num_E/NP
+        result = np.zeros((int(G)+1,34))
+
+        for i in range(31):
+            np.random.seed(i)
+            result[:,i] = evolution.DE_random_Y_binomial()
 
 
-# main
-NP = 10**3
-evolution = DifferentialEvolution(NP,1,1,1,rastrigen)
-G =  num_E/NP
-result = np.zeros((int(G),34))
+        for g in range(int(G)+1):
+            result[g,31] = np.average(result[g])
+            result[g,32] = np.median(result[g])
+            result[g,33] = np.std(result[g])
 
-for i in range(31):
-    np.random.seed(i)
-    result[:,i] = evolution.DE_random_Y_binomial()
+        with open(evolution.path,'wt') as cfile:
+            np.savetxt(cfile,result,delimiter=',')
+            np.savetxt(cfile,[datetime.datetime.now()],fmt="%s")
+        
 
-for g in range(int(G)):
-    result[:,31] = np.average(result[g])
-    result[:,32] = np.median(result[g])
-    result[:,33] = np.std(result[g])
 
-with open('result.csv','at') as cfile:
-    np.savetxt(cfile,result,delimiter=',')
-    np.savetxt(cfile,[datetime.datetime.now()],fmt="%s")
+# -----main-----
+
+evolution = Runner(100,1,1,1,rastrigen)
