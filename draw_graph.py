449a38b5709bb044278a8413e8f791ea82277a12
Aizin12 <absolutely1725@gmail.com>
2023-07-06 01:37:39 +0900

    グラフの追加

--- draw_graph.py
+++ draw_graph.py
@@ -10,21 +10,43 @@ parser = argparse.ArgumentParser()
 parser.add_argument("subject",nargs='*')
 args = parser.parse_args()
 
-#ファイル名から各種パラメータを取得する[目的関数,DE,ベースベクトルの選択方法,Y,交叉方法,NP,F,CR]
+#ファイル名から各種パラメータを取得する[目的関数,DE,ベースベクトルの選択方法,Y,交叉方法,NP,F,CR](実環境の場合)
 parameter = args.subject
-parameter = re.split(r'[/_]',parameter.replace('.csv',''))
+size = len(args.subject)
+for i in range(size):
+    parameter = re.split(r'[/_]',parameter[i].replace('.csv','')) 
 
-num_E = 10**4
-G = int(num_E/int(parameter[5]))
-result = np.zeros([G+1,34])
-with open(args.subject,"r")as fr:
-    for i in range(G+1):
-        result[i] = fr.readline().split(",")
+#ファイル読み取り
+def read_file(G,trails):
+    result = np.zeros([size,G+1,trails])
+    cnt = 0
+    for subject in args.subject:
+        with open(subject,"r")as fr:
+            for i in range(G+1):
+                result[cnt,i] = fr.readline().split(",")
+        cnt += 1
+    return result
 
 
-#箱ひげ図
-fig,ax = plt.subplots()
-bp = ax.boxplot(result[G,:31])
-ax.set_xticklabels([args.subject])
-plt.grid()
-plt.show()
+def make_scatterplot(result):
+    file_name = f"{parameter[0]}/{parameter[1]}_{parameter[6]}_{parameter[7]}_{parameter[2]}_NP={parameter[3]}_F={parameter[4]}_CR={parameter[5]}.png"
+    fig,ax = plt.subplots()
+    for i in range(size):
+        ax.plot(result[i,0,:],result[i,1,:],marker="o")
+    ax.set_xlabel(parameter[2])
+    ax.set_ylabel("fitness")
+    plt.savefig(file_name)
+
+
+
+#箱ひげ図作成
+def make_boxplot():
+    fig,ax = plt.subplots()
+    bp = ax.boxplot(result[G,:31])
+    ax.set_xticklabels([args.subject])
+    plt.grid()
+    plt.show()
+
+
+data = read_file(1,100)
+make_scatterplot(data)
